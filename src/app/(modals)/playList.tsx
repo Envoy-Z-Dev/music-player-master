636f7f13f183ec5a5c738151da138512fb49e82b
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 15:49:29 +0530

    Update playList.tsx

--- src/app/(modals)/playList.tsx
+++ src/app/(modals)/playList.tsx
@@ -1,29 +1,29 @@
-import { NowPlayList } from '@/components/NowPlayList'
-import { colors, screenPadding } from '@/constants/tokens'
-import { usePlayList } from '@/store/playList'
-import { defaultStyles } from '@/styles'
-import { useHeaderHeight } from '@react-navigation/elements'
-import React from 'react'
-import { StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Track } from 'react-native-track-player'
+import { NowPlayList } from '@/components/NowPlayList';
+import { colors, screenPadding } from '@/constants/tokens';
+import { usePlayList } from '@/store/playList';
+import { defaultStyles } from '@/styles';
+import { useHeaderHeight } from '@react-navigation/elements';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Track } from 'react-native-track-player';
 
 const PlayListScreen = () => {
-	const headerHeight = useHeaderHeight()
-	const tracks = usePlayList()
+	const headerHeight = useHeaderHeight();
+	const tracks = usePlayList();
 
 	return (
 		<SafeAreaView style={[styles.modalContainer, { paddingTop: headerHeight }]}>
 			<NowPlayList id="PlayListScreen" tracks={tracks as Track[]} />
 		</SafeAreaView>
-	)
-}
+	);
+};
 
 const styles = StyleSheet.create({
 	modalContainer: {
 		flex: 1,
 		paddingHorizontal: screenPadding.horizontal,
-		backgroundColor: defaultStyles.container.backgroundColor, // 设置默认背景颜色
+		backgroundColor: defaultStyles.container.backgroundColor,
 	},
 	header: {
 		fontSize: 28,
@@ -33,6 +33,6 @@ const styles = StyleSheet.create({
 		paddingTop: 0,
 		color: colors.text,
 	},
-})
+});
 
-export default PlayListScreen
+export default PlayListScreen;
