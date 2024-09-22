764e72b2a408b37b7bfaa6571959e31a111cf9dd
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 15:40:09 +0530

    Update _layout.tsx

--- src/app/(tabs)/favorites/_layout.tsx
+++ src/app/(tabs)/favorites/_layout.tsx
@@ -1,58 +1,63 @@
-import AddPlayListButton from '@/components/AddPlayListButton'
-import { StackScreenWithSearchBar } from '@/constants/layout'
-import { colors } from '@/constants/tokens'
-import { defaultStyles } from '@/styles'
-import { Stack } from 'expo-router'
-import { View } from 'react-native'
+import AddPlayListButton from '@/components/AddPlayListButton';
+import { StackScreenWithSearchBar } from '@/constants/layout';
+import { colors } from '@/constants/tokens';
+import { defaultStyles } from '@/styles';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const FavoritesScreenLayout = () => {
 	return (
-		<View style={defaultStyles.container}>
-			<Stack>
-				<Stack.Screen
-					name="index"
-					options={{
-						...StackScreenWithSearchBar,
-						headerTitle: 'Favorites',
-						headerRight: () => <AddPlayListButton />,
-					}}
-				/>
-				<Stack.Screen
-					name="[name]"
-					options={{
-						headerTitle: '',
-						headerBackVisible: true,
-						headerStyle: {
-							backgroundColor: colors.background,
-						},
-						headerTintColor: colors.primary,
-					}}
-				/>
-				<Stack.Screen
-					name="favoriteMusic"
-					options={{
-						headerTitle: '',
-						headerBackVisible: true,
-						headerStyle: {
-							backgroundColor: colors.background,
-						},
-						headerTintColor: colors.primary,
-					}}
-				/>
-				<Stack.Screen
-					name="localMusic"
-					options={{
-						headerTitle: '',
-						headerBackVisible: true,
-						headerStyle: {
-							backgroundColor: colors.background,
-						},
-						headerTintColor: colors.primary,
-					}}
-				/>
-			</Stack>
-		</View>
-	)
-}
+		<NavigationContainer>
+			<View style={defaultStyles.container}>
+				<Stack.Navigator>
+					<Stack.Screen
+						name="index"
+						options={{
+							...StackScreenWithSearchBar,
+							headerTitle: 'Favorites',
+							headerRight: () => <AddPlayListButton />,
+						}}
+					/>
+					<Stack.Screen
+						name="[name]"
+						options={{
+							headerTitle: '',
+							headerBackVisible: true,
+							headerStyle: {
+								backgroundColor: colors.background,
+							},
+							headerTintColor: colors.primary,
+						}}
+					/>
+					<Stack.Screen
+						name="favoriteMusic"
+						options={{
+							headerTitle: '',
+							headerBackVisible: true,
+							headerStyle: {
+								backgroundColor: colors.background,
+							},
+							headerTintColor: colors.primary,
+						}}
+					/>
+					<Stack.Screen
+						name="localMusic"
+						options={{
+							headerTitle: '',
+							headerBackVisible: true,
+							headerStyle: {
+								backgroundColor: colors.background,
+							},
+							headerTintColor: colors.primary,
+						}}
+					/>
+				</Stack.Navigator>
+			</View>
+		</NavigationContainer>
+	);
+};
 
-export default FavoritesScreenLayout
+export default FavoritesScreenLayout;
