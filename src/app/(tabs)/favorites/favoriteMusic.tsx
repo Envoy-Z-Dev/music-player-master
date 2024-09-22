9df801e46757506e6a282f37b5e38c31e69e9e23
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 15:39:02 +0530

    Update favoriteMusic.tsx

--- src/app/(tabs)/favorites/favoriteMusic.tsx
+++ src/app/(tabs)/favorites/favoriteMusic.tsx
@@ -1,34 +1,27 @@
-import { PlaylistTracksList } from '@/components/PlaylistTracksList'
-import { screenPadding } from '@/constants/tokens'
-import { Playlist } from '@/helpers/types'
-import { useFavorites } from '@/store/library'
-import { defaultStyles } from '@/styles'
-import React, { useMemo } from 'react'
-import { ScrollView, View } from 'react-native'
-import { Track } from 'react-native-track-player'
+import { PlaylistTracksList } from '@/components/PlaylistTracksList';
+import { screenPadding } from '@/constants/tokens';
+import { Playlist } from '@/helpers/types';
+import { useFavorites } from '@/store/library';
+import { defaultStyles } from '@/styles';
+import React, { useMemo } from 'react';
+import { ScrollView, View } from 'react-native';
+import { Track } from 'react-native-track-player';
 
 const FavoriteMusicScreen = () => {
-	// const search = useNavigationSearch({
-	// 	searchBarOptions: {
-	// 		placeholder: 'Find in favorites',
-	// 	},
-	// })
-
-	const { favorites } = useFavorites()
+	const { favorites } = useFavorites();
 	const playListItem = {
 		name: 'Favorites',
 		id: 'favorites',
 		tracks: [],
-		title: '喜欢的歌曲',
+		title: 'Favorite Songs',
 		coverImg: 'https://y.qq.com/mediastyle/global/img/cover_like.png?max_age=2592000',
-		description: '喜欢的歌曲',
-	}
-	const playLists = [playListItem]
-	const filteredFavoritesTracks = useMemo(() => {
-		// if (!search) return favorites as Track[]
+		description: 'Favorite Songs',
+	};
 
-		return favorites as Track[]
-	}, [favorites])
+	const playLists = [playListItem];
+	const filteredFavoritesTracks = useMemo(() => {
+		return favorites as Track[];
+	}, [favorites]);
 
 	return (
 		<View style={defaultStyles.container}>
@@ -39,6 +32,7 @@ const FavoriteMusicScreen = () => {
 				<PlaylistTracksList playlist={playListItem as Playlist} tracks={filteredFavoritesTracks} />
 			</ScrollView>
 		</View>
-	)
-}
-export default FavoriteMusicScreen
+	);
+};
+
+export default FavoriteMusicScreen;
