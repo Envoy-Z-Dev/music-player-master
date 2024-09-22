563a95491e8268d57f781d69d2e745141d0563c2
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 13:32:35 +0530

    Update index.tsx

--- src/app/(tabs)/search/index.tsx
+++ src/app/(tabs)/search/index.tsx
@@ -1,30 +1,30 @@
-import { SearchList } from '@/components/SearchList'
-import { screenPadding } from '@/constants/tokens'
-import searchAll from '@/helpers/searchAll'
-import { useNavigationSearch } from '@/hooks/useNavigationSearch'
-import { defaultStyles } from '@/styles'
-import { useRouter } from 'expo-router'
-import { useEffect, useState } from 'react'
-import { ScrollView, View } from 'react-native'
-import { Track } from 'react-native-track-player'
+import { SearchList } from '@/components/SearchList';
+import { screenPadding } from '@/constants/tokens';
+import searchAll from '@/helpers/searchAll';
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
+import { defaultStyles } from '@/styles';
+import { useNavigation } from '@react-navigation/native'; // Use react-navigation for navigation
+import { useEffect, useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import { Track } from 'react-native-track-player';
 
 const SearchlistsScreen = () => {
-	const router = useRouter()
-	const [searchResults, setSearchResults] = useState<Track[]>([])
+	const navigation = useNavigation();
+	const [searchResults, setSearchResults] = useState<Track[]>([]);
 
 	const search = useNavigationSearch({
 		searchBarOptions: {
 			placeholder: 'Find Something',
 		},
-	})
+	});
 
 	useEffect(() => {
 		const fetchSearchResults = async () => {
-			const results = await searchAll(search)
-			setSearchResults(results)
-		}
-		fetchSearchResults()
-	}, [search])
+			const results = await searchAll(search);
+			setSearchResults(results);
+		};
+		fetchSearchResults();
+	}, [search]);
 
 	return (
 		<View style={defaultStyles.container}>
@@ -37,7 +37,7 @@ const SearchlistsScreen = () => {
 				<SearchList scrollEnabled={false} tracks={searchResults} id={'search'} />
 			</ScrollView>
 		</View>
-	)
-}
+	);
+};
 
-export default SearchlistsScreen
+export default SearchlistsScreen;
