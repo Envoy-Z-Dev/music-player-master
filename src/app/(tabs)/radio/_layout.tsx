fe3d1d8ff839e7270667f29120faeed3e592e830
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 13:36:17 +0530

    Update _layout.tsx

--- src/app/(tabs)/radio/_layout.tsx
+++ src/app/(tabs)/radio/_layout.tsx
@@ -1,34 +1,39 @@
-import { StackScreenWithSearchBar } from '@/constants/layout'
-import { colors } from '@/constants/tokens'
-import { defaultStyles } from '@/styles'
-import { Stack } from 'expo-router'
-import { View } from 'react-native'
+import { StackScreenWithSearchBar } from '@/constants/layout';
+import { colors } from '@/constants/tokens';
+import { defaultStyles } from '@/styles';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const RadiolistsScreenLayout = () => {
 	return (
-		<View style={defaultStyles.container}>
-			<Stack>
-				<Stack.Screen
-					name="index"
-					options={{
-						...StackScreenWithSearchBar,
-						headerTitle: 'Radio',
-					}}
-				/>
-				<Stack.Screen
-					name="[name]"
-					options={{
-						headerTitle: '',
-						headerBackVisible: true,
-						headerStyle: {
-							backgroundColor: colors.background,
-						},
-						headerTintColor: colors.primary,
-					}}
-				/>
-			</Stack>
-		</View>
-	)
-}
+		<NavigationContainer>
+			<View style={defaultStyles.container}>
+				<Stack.Navigator>
+					<Stack.Screen
+						name="index"
+						options={{
+							...StackScreenWithSearchBar,
+							headerTitle: 'Radio',
+						}}
+					/>
+					<Stack.Screen
+						name="[name]"
+						options={{
+							headerTitle: '',
+							headerBackVisible: true,
+							headerStyle: {
+								backgroundColor: colors.background,
+							},
+							headerTintColor: colors.primary,
+						}}
+					/>
+				</Stack.Navigator>
+			</View>
+		</NavigationContainer>
+	);
+};
 
-export default RadiolistsScreenLayout
+export default RadiolistsScreenLayout;
