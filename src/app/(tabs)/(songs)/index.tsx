cbba5b2d9e21346314255a62ce264856781f6886
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 15:42:25 +0530

    Update index.tsx

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,27 +1,27 @@
-import { TracksList } from '@/components/TracksList'
-import { screenPadding } from '@/constants/tokens'
-import { trackTitleFilter } from '@/helpers/filter'
-import { generateTracksListId } from '@/helpers/miscellaneous'
-import { useNavigationSearch } from '@/hooks/useNavigationSearch'
-import { useTracks } from '@/store/library'
-import { defaultStyles } from '@/styles'
-import { useMemo } from 'react'
-import { ScrollView, View } from 'react-native'
+import { TracksList } from '@/components/TracksList';
+import { screenPadding } from '@/constants/tokens';
+import { trackTitleFilter } from '@/helpers/filter';
+import { generateTracksListId } from '@/helpers/miscellaneous';
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
+import { useTracks } from '@/store/library';
+import { defaultStyles } from '@/styles';
+import { useMemo } from 'react';
+import { ScrollView, View } from 'react-native';
 
 const SongsScreen = () => {
 	const search = useNavigationSearch({
 		searchBarOptions: {
 			placeholder: 'Find in songs',
 		},
-	})
+	});
 
-	const tracks = useTracks()
+	const tracks = useTracks();
 
 	const filteredTracks = useMemo(() => {
-		if (!search) return tracks
+		if (!search) return tracks;
 
-		return tracks.filter(trackTitleFilter(search))
-	}, [search, tracks])
+		return tracks.filter(trackTitleFilter(search));
+	}, [search, tracks]);
 
 	return (
 		<View style={defaultStyles.container}>
@@ -36,7 +36,7 @@ const SongsScreen = () => {
 				/>
 			</ScrollView>
 		</View>
-	)
-}
+	);
+};
 
-export default SongsScreen
+export default SongsScreen;
