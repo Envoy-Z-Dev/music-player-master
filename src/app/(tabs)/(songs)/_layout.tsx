e617cdda99753ce643de9fc557ff49772a4dbe3e
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 15:43:32 +0530

    Update _layout.tsx

--- src/app/(tabs)/(songs)/_layout.tsx
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -1,24 +1,29 @@
-import GlobalButton from '@/components/GlobalButton'
-import { StackScreenWithSearchBar } from '@/constants/layout'
-import { defaultStyles } from '@/styles'
-import { Stack } from 'expo-router'
-import { View } from 'react-native'
+import GlobalButton from '@/components/GlobalButton';
+import { StackScreenWithSearchBar } from '@/constants/layout';
+import { defaultStyles } from '@/styles';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const SongsScreenLayout = () => {
 	return (
-		<View style={defaultStyles.container}>
-			<Stack>
-				<Stack.Screen
-					name="index"
-					options={{
-						...StackScreenWithSearchBar,
-						headerTitle: 'Songs',
-						headerRight: () => <GlobalButton />,
-					}}
-				/>
-			</Stack>
-		</View>
-	)
-}
+		<NavigationContainer>
+			<View style={defaultStyles.container}>
+				<Stack.Navigator>
+					<Stack.Screen
+						name="index"
+						options={{
+							...StackScreenWithSearchBar,
+							headerTitle: 'Songs',
+							headerRight: () => <GlobalButton />,
+						}}
+					/>
+				</Stack.Navigator>
+			</View>
+		</NavigationContainer>
+	);
+};
 
-export default SongsScreenLayout
+export default SongsScreenLayout;
