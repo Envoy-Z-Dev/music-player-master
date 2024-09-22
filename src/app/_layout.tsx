53db25fe03592d87e580ede8f1a5da8172652655
Ꮛnѵoყ-Z <152428796+Envoy-Z-Lab@users.noreply.github.com>
2024-09-22 13:29:10 +0530

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,29 +1,29 @@
-import { playbackService } from '@/constants/playbackService'
-import { colors } from '@/constants/tokens'
-import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState'
-import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer'
-import { SplashScreen, Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
-import { useCallback } from 'react'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import TrackPlayer from 'react-native-track-player'
+import { playbackService } from '@/constants/playbackService';
+import { colors } from '@/constants/tokens';
+import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState';
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer';
+import { StatusBar } from 'react-native-status-bar';
+import { useCallback } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TrackPlayer from 'react-native-track-player';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-SplashScreen.preventAutoHideAsync()
+TrackPlayer.registerPlaybackService(() => playbackService);
 
-TrackPlayer.registerPlaybackService(() => playbackService)
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 	const handleTrackPlayerLoaded = useCallback(() => {
-		setTimeout(SplashScreen.hideAsync, 1500)
-	}, [])
+		setTimeout(() => {}, 1500);
+	}, []);
 
 	useSetupTrackPlayer({
-		onLoad: handleTrackPlayerLoaded, //播放器初始化后调用这个回调函数。这里先传过去。
-	})
+		onLoad: handleTrackPlayerLoaded,
+	});
 
-	useLogTrackPlayerState()
-	// myTrackPlayer.setupTrackPlayer()
+	useLogTrackPlayerState();
 
 	return (
 		<SafeAreaProvider>
@@ -32,92 +32,93 @@ const App = () => {
 				<StatusBar style="auto" />
 			</GestureHandlerRootView>
 		</SafeAreaProvider>
-	)
-}
+	);
+};
 
 const RootNavigation = () => {
 	return (
-		//每个 Stack.Screen 组件定义了一个可导航的屏幕
-		<Stack>
-			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-			<Stack.Screen
-				name="player"
-				options={{
-					presentation: 'card',
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-					animationDuration: 400,
-					headerShown: false,
-				}}
-			/>
-			<Stack.Screen
-				name="(modals)/playList"
-				options={{
-					presentation: 'modal',
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-					animationDuration: 400,
-					headerShown: false,
-				}}
-			/>
-			<Stack.Screen
-				name="(modals)/addToPlaylist"
-				options={{
-					presentation: 'modal',
-					headerStyle: {
-						backgroundColor: colors.background,
-					},
-					headerTitle: '添加到歌单',
-					headerTitleStyle: {
-						color: colors.text,
-					},
-				}}
-			/>
-			<Stack.Screen
-				name="(modals)/settingModal"
-				options={{
-					presentation: 'modal',
-					headerShown: false,
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-				}}
-			/>
-			<Stack.Screen
-				name="(modals)/importPlayList"
-				options={{
-					presentation: 'modal',
-					headerShown: false,
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-				}}
-			/>
-			<Stack.Screen
-				name="(modals)/[name]"
-				options={{
-					presentation: 'modal',
-					headerShown: false,
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-				}}
-			/>
-			<Stack.Screen
-				name="(modals)/logScreen"
-				options={{
-					presentation: 'modal',
-					headerShown: true,
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-					headerTitle: '应用日志',
-					headerStyle: {
-						backgroundColor: colors.background,
-					},
-					headerTitleStyle: {
-						color: colors.text,
-					},
-				}}
-			/>
-		</Stack>
-	)
-}
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen
+					name="player"
+					options={{
+						presentation: 'card',
+						gestureEnabled: true,
+						gestureDirection: 'vertical',
+						animationDuration: 400,
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen
+					name="(modals)/playList"
+					options={{
+						presentation: 'modal',
+						gestureEnabled: true,
+						gestureDirection: 'vertical',
+						animationDuration: 400,
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen
+					name="(modals)/addToPlaylist"
+					options={{
+						presentation: 'modal',
+						headerStyle: {
+							backgroundColor: colors.background,
+						},
+						headerTitle: 'Add to Playlist',
+						headerTitleStyle: {
+							color: colors.text,
+						},
+					}}
+				/>
+				<Stack.Screen
+					name="(modals)/settingModal"
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+						gestureEnabled: true,
+						gestureDirection: 'vertical',
+					}}
+				/>
+				<Stack.Screen
+					name="(modals)/importPlayList"
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+						gestureEnabled: true,
+						gestureDirection: 'vertical',
+					}}
+				/>
+				<Stack.Screen
+					name="(modals)/[name]"
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+						gestureEnabled: true,
+						gestureDirection: 'vertical',
+					}}
+				/>
+				<Stack.Screen
+					name="(modals)/logScreen"
+					options={{
+						presentation: 'modal',
+						headerShown: true,
+						gestureEnabled: true,
+						gestureDirection: 'vertical',
+						headerTitle: 'Application Log',
+						headerStyle: {
+							backgroundColor: colors.background,
+						},
+						headerTitleStyle: {
+							color: colors.text,
+						},
+					}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+    );
+};
 
-export default App
+export default App;
